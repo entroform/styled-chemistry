bc36aa7cbfc70fbd37311e063df349dc31f91d84
Andrew Prasetya <andrew@donkeymob.local>
2020-01-30 13:32:18 -0500

    Cleaned up tsconfig and rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
+import { terser } from 'rollup-plugin-terser';
 import babel from 'rollup-plugin-babel';
 import external from "rollup-plugin-peer-deps-external";
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
 
